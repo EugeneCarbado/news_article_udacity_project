e9adbd1d1a8dce05d3028eb30acdaf5b91e88205
eugene <eugenecarbado@gmail.com>
2021-02-16 13:43:53 +0000

    .

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -27,23 +27,9 @@ function handleSubmit(event) {
     }
 
     console.log("::: Form Submitted :::")
-
-    fetch("http://localhost:8080/article", {
-            method: "POST",
-            cache: "no-cache",
-            credentials: "same-origin",
-            headers: {
-                "Content-Type": "text/plain",
-            },
-            body: formUrl,
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            updateUI(res);
-        })
 }
 
-async function updateUI() {
+async function updateUI(res) {
     // GET function that takes the info from the server
     document.querySelector('#result_output').innerText = res.message;
     document.querySelector('#subjectivity_output').innerText = res.subjectivity;
